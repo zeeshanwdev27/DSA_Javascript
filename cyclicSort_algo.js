433b6e1e259bdedf67b353c7a0c53be60030cfc8
zeeshanwdev27 <zeeshanwdev27@gmail.com>
2026-06-20 01:06:22 +0500

    added another exp question

--- cyclicSort_algo.js
+++ cyclicSort_algo.js
@@ -1,3 +1,5 @@
+
+/*
 let arr = [6, 5, 8, 2, 1, 4, 3, 7]
 let i = 0
 
@@ -17,3 +19,37 @@ while(i<arr.length){
 
 
 console.log(arr);
+
+*/
+
+
+
+
+// Question 2:  find missing number from the array 
+
+let arr = [3,0,1]
+
+function missingNumber (num){
+    
+    let i=0;
+
+    while(i<num.length){
+        let correctIdx = num[i]
+        if(num[i]<num.length && num[i] != num[correctIdx]){
+            let temp = num[i]
+            num[i] = num[correctIdx]
+            num[correctIdx] = temp
+        }
+        else i++
+    }
+
+    for(let j=0; j<num.length; j++){
+        if(j != num[j]) return j
+    }
+
+    return num.length;
+
+}
+
+
+console.log(missingNumber(arr))
